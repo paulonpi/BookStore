4a3f4282d5f0bbcee95cdd28b9c2ac9749aa034c
paulo-gral <paulo.gral@akadseguros.com.br>
2024-12-11 20:37:43 -0300

    Feat config route

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ListComponent } from './modules/book/list/list.component';
+
+export const routes: Routes = [
+  {
+    path: 'books',
+    component: ListComponent,
+  },
+];
